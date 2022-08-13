c034975d040d81141a47c79647da618e77b83461
retaj22 <mineraali8@gmail.com>
2022-08-13 13:51:13 +0300

    my first wed test

---
+++ test.js
@@ -0,0 +1,33 @@
+alert("wellcome ")
+
+
+let grade = prompt('enttr your grade')
+
+
+if(grade >=90 && grade <= 100) 
+{
+
+console.log ("لقد حصلت على امتياز")
+}
+else if(grade >= 80 && grade <= 89)
+{
+console.log("لقد حصلت على جيد جداً🤩")
+}
+else if( grade >=70 && grade <= 79)
+{
+console.log( "لقد حصلت على جيد")
+} else if (grade >= 60 && grade <= 69)
+{
+    console.log("لقد حصلت على مقبول") 
+    
+ }
+ else if(grade >= 50 && grade <= 59 ){ 
+    console.log("لقد حصلت على ضعيف ")
+ }
+ else if( grade < 50 )
+ {
+    console.log("راسب")
+ }
+ 
+
+
